d70e753fefa4627c7abe52f43a38d0aed6fdc33d
Suoy Sovichea <suoysovichea@Suoys-MacBook-Air.local>
2025-03-05 12:11:23 +0700

    change footer and image url

--- admin/products.php
+++ admin/products.php
@@ -69,8 +69,7 @@ $pdo = $db->getConnection();
                                                 <td><?php echo $product['name']; ?></td>
                                                 <td>$<?php echo number_format($product['price'], 2); ?></td>
                                                 <td>
-                                                    <img src="<?php echo $product['image']; ?>" alt="Product Image"
-                                                        width="50">
+                                                <img src="/www/web_project/<?php echo $product['image']; ?>" width="50">
                                                 </td>
                                                 <td><?php echo substr($product['description'], 0, 50) . '...'; ?></td>
                                                 <td><?php echo $product['category']; ?></td>
@@ -100,7 +99,6 @@ $pdo = $db->getConnection();
                         </div>
                     </div>
                 </div>
-                <?php include 'includes/footer.php'; ?>
             </div>
         </div>
     </div>
